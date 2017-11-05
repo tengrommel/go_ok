15a29deb64e1176a81a65768c3f28c12ab93d0d6
teng <wwwsousoucom@126.com>
2017-11-05 20:26:40 +0800

    ok

--- src/main/main.go
+++ src/main/main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
-	"github.com/labstack/echo"
 	"net/http"
+	"github.com/labstack/echo"
 )
+
+func yallo(c echo.Context) error {
+		return c.String(http.StatusOK, "yallo from the web side!")
+}
 
 func main(){
 	fmt.Println("welcome to the server")
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "yallo from the web side!")
-	})
+	e.GET("/", yallo)
 	e.Start(":8000")
 }
