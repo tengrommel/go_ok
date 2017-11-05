1206db921808322e457eb2afbff0bd441cfd593c
teng <wwwsousoucom@126.com>
2017-11-05 21:57:52 +0800

    echo BIND

--- src/main/main.go
+++ src/main/main.go
@@ -4,7 +4,25 @@ import (
 	"fmt"
 	"net/http"
 	"github.com/labstack/echo"
+	"io/ioutil"
+	"log"
+	"encoding/json"
 )
+
+type Cat struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+type Dog struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+type Hamster struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
 
 func yallo(c echo.Context) error {
 		return c.String(http.StatusOK, "yallo from the web side!")
@@ -30,10 +48,54 @@ func getCats(c echo.Context) error{
 	})
 }
 
+func addCat(c echo.Context) error {
+	cat := Cat{}
+	defer c.Request().Body.Close()
+	b, err := ioutil.ReadAll(c.Request().Body)
+	if err != nil {
+		log.Printf("Failed reading the request body: %s", err)
+		return c.String(http.StatusInternalServerError, "")
+	}
+	err = json.Unmarshal(b, &cat)
+	if err != nil {
+		log.Printf("Failed unmarshaling in addCats: %s", err)
+		return c.String(http.StatusInternalServerError, "")
+	}
+	log.Printf("this is your cat: %#v", cat)
+	return c.String(http.StatusOK, "we got your cat!")
+}
+
+
+func addDog(c echo.Context) error {
+	dog := Dog{}
+	defer c.Request().Body.Close()
+	err := json.NewDecoder(c.Request().Body).Decode(&dog)
+	if err != nil{
+		log.Printf("Failed processing addDog request: %s", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+	log.Printf("this is your dog: %#v", dog)
+	return c.String(http.StatusOK, "we got your dog!")
+}
+
+func addHamster(c echo.Context) error {
+	hamster := Hamster{}
+	err := c.Bind(&hamster)
+	if err != nil{
+		log.Printf("Failed processing addHamster request: %s", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+	log.Printf("this is your hamster: %#v", hamster)
+	return c.String(http.StatusOK, "we got your hamster!")
+}
+
 func main(){
 	fmt.Println("welcome to the server")
 	e := echo.New()
 	e.GET("/", yallo)
 	e.GET("/cats/:data", getCats)
+	e.POST("/cats", addCat)
+	e.POST("/dogs", addDog)
+	e.POST("/hamsters", addHamster)
 	e.Start(":8000")
 }
